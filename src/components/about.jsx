b099bc02aa29178907b423d95cd2ccb544527703
Yongrui Su <sam.ysu.uc@gmail.com>
2020-04-18 19:05:03 -0700

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,10 +11,10 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am a fulltime Software Engineer working in Google focusing on the Video Search features of www.google.com. Like most of engineers in the Silicon Valley, I love exploring new technologies and often amazed by the technoligical progress made so far in the recent years.</p>
+                    <p>As a Molecular Engineering Ph.D. who later transferred to the semiconductor and the Internet industry, I have hands-on experience with lots of technologies ranging from Molecular Simulation, Monte Carlo Simulation, High-Performance Computer Simulation, Large-Scale Image Processing(for example, OPC image processing), Google-Scale Search Ranking and Frontend Web Development.</p>
                     </div>
                 </div>
                 </div>
@@ -27,7 +27,7 @@ export default class About extends Component {
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <h2 className="colorlib-heading">Here are some of my selected expertise</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -38,7 +38,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <p>As a full-stack Software Engineer working in Google, I can work on both the frontend and the backend to build low-latency, highly stable websites and chrome extentions using Java, C++, JavaScript,HTML,CSS.</p>
                 </div>
                 </div>
             </div>
@@ -48,8 +48,8 @@ export default class About extends Component {
                     <i className="icon-phone3" />
                 </span>
                 <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <h3>High-performance simulation</h3>
+                    <p>With a Ph.D. degree focusing on the field of Molecular Simulation,  I am able to develop high-performance softwares simulating phyiscal systems composed by millions of moleculars using C++ and CUDA.</p>
                 </div>
                 </div>
             </div>
@@ -59,8 +59,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Large-Scale image processing</h3>
+                    <p>I am able to develop machine-learning based image-processing software to detect and correct patterns on millions of images. This technology is called Optical Proximity Correction(OPC) in the semiconductor industry.</p>
                 </div>
                 </div>
             </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -9,7 +9,7 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta">highlights</span>
-                <h2 className="colorlib-heading animate-box">Timeline</h2>
+                <h2 className="colorlib-heading animate-box">Timeline After Undergraduate</h2>
               </div>
             </div>
             <div className="row">
@@ -21,7 +21,7 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
+                        <h2>Google <span>Apr. 2019-present</span></h2>
                         <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
                       </div>
                     </div>
@@ -32,8 +32,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2>Samsung Semiconductor Inc. <span>Feb. 2017-Apri. 2019</span></h2>
+                        <p>I joined the Samsung Semiconductor Inc. to develop</p>
                       </div>
                     </div>
                   </article>
@@ -43,8 +43,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2>Ph.D. Education <span>Sep. 2011-Dec. 2016</span></h2>
+                        <p>After obtaining a Master degree in Chemical Engineering from the University of Wisconsin-Madison in 2013, I transferred to the University of Chicago with my Ph.D. advisor <a href="https://pme.uchicago.edu/faculty/juan-de-pablo">Juan J. de Pablo</a> to build the <a href="https://pme.uchicago.edu/">Pritzker School of Molecular Engineering</a>, which is the first engineering school in the university's history. My Ph.D. <a href="https://knowledge.uchicago.edu/record/661/files/Su_uchicago_0330D_13557.pdf">thesis</a> focuses on the extreme scale simulation of nanoparticle/polymer systems by Monte Carlo and Molecule dynamics in order to understand the behavoir of polymer systems from the molecular level.</p> 
                       </div>
                     </div>
                   </article>
