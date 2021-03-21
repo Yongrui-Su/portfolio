7aef3de42431bf9b1c0984653f8d379975bdc414
Yongrui Su <sam.ysu.uc@gmail.com>
2021-03-21 01:04:26 -0700

    remove resume link

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Yongrui(Samuel)</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1XDEWUca5KSTLm0vXy99gMyzUh_KzqHhD/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          {/*}<p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1XDEWUca5KSTLm0vXy99gMyzUh_KzqHhD/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>*/}
                         </div>
                       </div>
                     </div>
